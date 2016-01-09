6399f80b2e8aa94262cdad4b925d7f592bb9197a
Himanshu Gupta <g.himanshu@gmail.com>
2016-01-09 13:02:14 -0600

    simplifying [Logging/Http]EmitterConfigTest and removing Emitters class

--- src/test/java/com/metamx/emitter/core/HttpEmitterConfigTest.java
+++ src/test/java/com/metamx/emitter/core/HttpEmitterConfigTest.java
@@ -17,25 +17,20 @@
 package com.metamx.emitter.core;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Properties;
 
 public class HttpEmitterConfigTest
 {
   @Test
-  public void testDefaults()
+  public void testDefaults() throws Exception
   {
-    final Properties props = new Properties();
-    props.put("com.metamx.emitter.http.url", "http://example.com/");
-
     final ObjectMapper objectMapper = new ObjectMapper();
-    final HttpEmitterConfig config = objectMapper.convertValue(Emitters.makeHttpMap(props), HttpEmitterConfig.class);
+    final HttpEmitterConfig config = objectMapper.readValue("{}", HttpEmitterConfig.class);
 
     Assert.assertEquals(60000, config.getFlushMillis());
-    Assert.assertEquals(300, config.getFlushCount());
-    Assert.assertEquals("http://example.com/", config.getRecipientBaseUrl());
+    Assert.assertEquals(500, config.getFlushCount());
+    Assert.assertNull(config.getRecipientBaseUrl());
     Assert.assertEquals(null, config.getBasicAuthentication());
     Assert.assertEquals(BatchingStrategy.ARRAY, config.getBatchingStrategy());
     Assert.assertEquals(5 * 1024 * 1024, config.getMaxBatchSize());
@@ -44,20 +39,28 @@ public class HttpEmitterConfigTest
   }
 
   @Test
-  public void testSettingEverything()
+  public void testSettingEverything() throws Exception
   {
-    final Properties props = new Properties();
-    props.setProperty("com.metamx.emitter.flushMillis", "1");
-    props.setProperty("com.metamx.emitter.flushCount", "2");
-    props.setProperty("com.metamx.emitter.http.url", "http://example.com/");
-    props.setProperty("com.metamx.emitter.http.basicAuthentication", "a:b");
-    props.setProperty("com.metamx.emitter.http.batchingStrategy", "newlines");
-    props.setProperty("com.metamx.emitter.http.maxBatchSize", "4");
-    props.setProperty("com.metamx.emitter.http.maxBufferSize", "8");
-    props.setProperty("com.metamx.emitter.http.flushTimeOut", "1000");
-
     final ObjectMapper objectMapper = new ObjectMapper();
-    final HttpEmitterConfig config = objectMapper.convertValue(Emitters.makeHttpMap(props), HttpEmitterConfig.class);
+    final HttpEmitterConfig config =
+        objectMapper.readValue(
+            objectMapper.writeValueAsString(
+                objectMapper.readValue(
+                    "{\n"
+                    + "  \"flushMillis\": 1,\n"
+                    + "  \"flushCount\": 2,\n"
+                    + "  \"flushTimeOut\": 1000,\n"
+                    + "  \"recipientBaseUrl\": \"http://example.com/\",\n"
+                    + "  \"basicAuthentication\": \"a:b\",\n"
+                    + "  \"batchingStrategy\": \"NEWLINES\",\n"
+                    + "  \"maxBatchSize\": 4,\n"
+                    + "  \"maxBufferSize\": 8\n"
+                    + "}",
+                    HttpEmitterConfig.class
+                )
+            ),
+            HttpEmitterConfig.class
+        );
 
     Assert.assertEquals(1, config.getFlushMillis());
     Assert.assertEquals(2, config.getFlushCount());
