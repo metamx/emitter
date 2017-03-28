dd873feb0ef8546d48e6d697f0847b0dfdc2acef
Ivan Simonenko <simonenko.nsk@gmail.com>
2017-03-28 12:58:49 -0700

    minor fixes

--- src/test/java/com/metamx/emitter/core/ParametrizedUriHttpPostEmitterTest.java
+++ src/test/java/com/metamx/emitter/core/ParametrizedUriHttpPostEmitterTest.java
@@ -30,8 +30,8 @@ public class ParametrizedUriHttpPostEmitterTest
 {
   private static final ObjectMapper jsonMapper = new ObjectMapper();
 
-  MockHttpClient httpClient;
-  Lifecycle lifecycle;
+  private MockHttpClient httpClient;
+  private Lifecycle lifecycle;
 
   @Before
   public void setUp() throws Exception
@@ -47,7 +47,7 @@ public class ParametrizedUriHttpPostEmitterTest
     }
   }
 
-  Emitter parametrizedEmmiter(String uriPattern) throws Exception
+  private Emitter parametrizedEmmiter(String uriPattern) throws Exception
   {
     final Properties props = new Properties();
     props.setProperty("com.metamx.emitter.parametrized", "true");
